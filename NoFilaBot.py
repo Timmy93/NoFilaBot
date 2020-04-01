3c68c8a5f916c6e81515df0761f92e3e54c4c2b8
Timmy93 <federico.seri@mail.polimi.it>
2020-04-01 21:07:05 +0200

    Bugfix

--- NoFilaBot.py
+++ NoFilaBot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import json
-
 import os
 import logging
 import yaml
@@ -15,7 +14,6 @@ import time
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from subprocess import call
-from TelegramMessage import TelegramMessage
 
 #Check if the given path is an absolute path
 def createAbsolutePath(path):
@@ -30,44 +28,40 @@ class NoFilaBot:
 	
 	#Load config and psw
 	def __init__(self, config, loggingHandler):
+		all_settings_dir 	= "Settings"
+		contact_list_path	= "myContactList.json"
+		
 		self.config = config
 		self.logging = loggingHandler
 		
 		#Loading values
-		self.readLocalParameters(config['local'])
-		self.readServerParameters(config['server'])
-		self.readMySupermarket(config['supermarkets'])
+		self.localParameters = config['local']
+		self.serverInfo = config['server']
+		self.mySupermarkets = config['supermarkets']
+		self.mySupermarketsList = self.getMySuperMarket()
+		self.logging.info('Supermarkets list extracted')
+		self.readContactList(createAbsolutePath(os.path.join(all_settings_dir,contact_list_path)))
 		
 		#Connecting to Telegram
 		self.TmUpdater = Updater(self.localParameters['telegram_token'], use_context=True)
 		self.TmDispatcher = self.TmUpdater.dispatcher
 		self.bot = self.TmUpdater.bot
 		self.logging.info("Connected succesfully to Telegram")
-	
-	#Read local parameters
-	def readLocalParameters(self, path):
-		self.localParameters = json.load(path)
-		self.logging.info('Local settings loaded')
-		return self.localParameters
-	
-	#Read the server parameters
-	def readServerParameters(self, path):
-		self.serverInfo = json.load(path)
-		self.logging.info('Server settings loaded')
-		return self.serverInfo
-	
-	#Read the personal list of supermarket
-	def readMySupermarket(self, path):
-		self.mySupermarkets = json.load(path)
-		self.logging.info('My supermarkets loaded')
-		self.mySupermarketsList = self.getMySuperMarket()
-		self.logging.info('Supermarkets list extracted')
-		return self.mySupermarkets
-	
+
 	#Read my contact list
 	def readContactList(self, path):
-		self.myContactList = json.load(path)
-		self.logging.info('My contact list loaded')
+		try:
+			with open(path) as json_file:
+				self.myContactList = json.load(json_file)
+		except ValueError:
+			self.logging.warning('Cannot decode the stored contact list - Using an empty one')
+			print("Invalid json ["+str(path)+"] - Use empty one")
+			self.myContactList = []
+		except FileNotFoundError:
+			self.logging.warning('Stored contact list not found- Using an empty one')
+			print("Contact list not existet ["+str(path)+"] - Use empty one")
+			self.myContactList = []
+		self.logging.info('Contact list loaded')
 		return self.myContactList
 	
 	#The complete function that iterate over all values
@@ -79,26 +73,34 @@ class NoFilaBot:
 		self.logging.info('Found '+str(len(relevant))+' relevant updates')
 		for relSup in relevant:
 			self.sendNotify(relSup['name'], relSup['minutes'], relSup['people'])
+		if not len(relevant):
+			self.sendNotify("Nessun aggiornamento rilevante, continuo a monitorare")
 		
 	#Send the request to the server to update the list of open supermarket
 	def requestUpdateSupermarkets(self):
 		payload = {'lat': self.serverInfo['lat'], 'long': self.serverInfo['long'], 'debug': self.serverInfo['debug']}
-		headers = {
+		h = {
 			'User-Agent': self.serverInfo['user_agent'],
-			'Accept': 'application/json, text/javascript, */*; q=0.01' 
-			'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' 
-			'Content-Type': 'application/json' 
-			'Origin': 'https://filaindiana.it' 
+			'Accept': 'application/json, text/javascript, */*; q=0.01',
+			'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+			'Content-Type': 'application/json',
+			'Origin': 'https://filaindiana.it',
 			'Referer': 'https://filaindiana.it/' 
 		}
-		r = requests.get(self.serverInfo['server_url'], params=payload)
+		r = requests.post(self.serverInfo['server_url'], data=json.dumps(payload), headers=h)
+		# ~ r = requests.post(self.serverInfo['server_url'], data=json.dumps(payload))
+		
+		# ~ self.logging.info(str(payload))
+		# ~ self.logging.info(str(self.serverInfo['server_url']))
+		# ~ self.logging.info("Response received ["+r.text+"]")
+		
 		return r.json()
 		
 	#Parse all the give supermarkets to discover relevant updates
 	def parseAllSupermarkets(self, dct):
 		relevant = []
 		for sm in dct:
-			if 'state' in dct and (sm['state']['queue_wait_minutes'] < self.localParameters['max_wait'] or sm['state']['queue_size_people'] < self.localParameters['max_people']):
+			if 'state' in sm and (sm['state']['queue_wait_minutes'] < self.localParameters['max_wait'] or sm['state']['queue_size_people'] < self.localParameters['max_people']):
 				lastUpdate = self.parseTime(sm['state']['updated_at'])
 				elapsed = datetime.now() - lastUpdate
 				#Check if enough time has passed
@@ -109,11 +111,18 @@ class NoFilaBot:
 						'minutes': sm['state']['queue_wait_minutes'], 
 						'people': sm['state']['queue_size_people']
 					})
+				else:
+					self.logging.info("Elapsed "+str(elapsed.total_seconds())+" seconds from last update - Skip")
+			else:
+				if not 'state' in sm:
+					self.logging.info("This supermarket has never received an update")
+				else:
+					self.logging.info("Supermarket - People: "+str(sm['state']['queue_size_people'])+" - Wait: "+str(sm['state']['queue_wait_minutes']))
 		return relevant
 	
 	#Parse the time in the log
 	def parseTime(self, timeString):
-		timeFormat = '%Y-%m-%d %H:%M:%S.%f'
+		timeFormat = '%Y-%m-%d %H:%M:%S'
 		parsed = datetime.strptime(timeString, timeFormat)
 		return parsed
 	
@@ -141,7 +150,8 @@ class NoFilaBot:
 	def start(self):
 		#Defining handlers
 		self.createHandlers()
-		print("Handlers created!")
+		self.logging.info("Bot handlers created")
+		print("Bot handlers created")
 		#Starting bot
 		self.TmUpdater.start_polling()
 		self.logging.info("Bot is now polling for new messages")
@@ -153,31 +163,15 @@ class NoFilaBot:
 	def createHandlers(self):
 		#Commands
 		self.TmDispatcher.add_handler(CommandHandler("start", self.welcomeMessage))
-		self.TmDispatcher.add_handler(CommandHandler("reboot", self.rebootHandler))
-		self.TmDispatcher.add_handler(CommandHandler("log", self.logHandler))
 		self.logging.info("createHandlers - Created handlers for command")
 		#Text message
-		self.TmDispatcher.add_handler(MessageHandler(Filters.text, self.searchHandler))
+		self.TmDispatcher.add_handler(MessageHandler(Filters.text, self.manageText))
 		self.logging.info("createHandlers - Created handlers for text")
 	
 	#Handle a received message
-	def handleMessage(self, msg):
-		self.message = TelegramMessage(msg, self.logging)
-		if self.message.isText():
-			self.handleTextMessage()
-		
-	#Handle different text messages
-	def handleTextMessage(self):
-		if self.message.hasThisText("/start"):
-			self.welcomeMessage()
-		elif self.message.hasThisText("/reboot") and self.isAdmin(self.message.getChat()):
-			self.sendMessage("Riavvio...")
-			risultato = str(subprocess.Popen("sudo reboot", shell=True, stdout=subprocess.PIPE).stdout.read())
-		elif self.message.hasThisText("/log") and self.isAdmin(self.message.getChat()):
-			self.analyseLog()
-		else:
-			releases = self.getRelease(self.message.getText())
-			self.printReleases(self.message.getChat(), releases)	
+	def manageText(self, msg):
+		self.loggin.info("Received text message - Ignoring")
+		print("Press /start to enable this bot")
 	
 	#Answers with a welcome message
 	def welcomeMessage(self, update=None, context=None):
@@ -185,10 +179,5 @@ class NoFilaBot:
 		if update.effective_chat.id in self.myContactList:
 			update.message.reply_text("Bentornato " + str(update.effective_chat.first_name))
 		else:
-			#Sleep to avoid misuse of the bot
-			#time.sleep(2)
+			self.myContactList.append(update.effective_chat.id)
 			update.message.reply_text("Ciao "+str(update.effective_chat.first_name)+", da adesso sarai aggiornati sulla fila dei supermercati nei dintorni")
-		
-	#Search a predefined release
-	def getRelease(self, title):
-		return self.apiHandler.searchRelease(title)
--- no_fila_bot.py
+++ no_fila_bot.py
@@ -2,7 +2,6 @@
 import sys
 import os
 import logging
-import yaml
 import subprocess
 import time
 import random
@@ -23,15 +22,18 @@ def createAbsolutePath(path):
 		
 	return path
 
+def test():
+	print("Sono stato chiamato!")
+
 #Defining local parameters
 all_settings_dir 	= "Settings"
 local_path 			= "local_settings.json"
 server_path 		= "server_settings.json"
-monitored_path 		= "monitored_markets.json"
-logFile		 		= "NoFilaBot.json"
-local_path 			= createAbsolutePath(os.path.join("all_settings_dir", local_path))
-server_path 		= createAbsolutePath(os.path.join("all_settings_dir", server_path))
-monitored_path 		= createAbsolutePath(os.path.join("all_settings_dir", monitored_path))
+monitored_path 		= "monitored_market.json"
+logFile		 		= "NoFilaBot.log"
+local_path 			= createAbsolutePath(os.path.join(all_settings_dir, local_path))
+server_path 		= createAbsolutePath(os.path.join(all_settings_dir, server_path))
+monitored_path 		= createAbsolutePath(os.path.join(all_settings_dir, monitored_path))
 logFile 			= createAbsolutePath(logFile)
 
 #Set logging file
@@ -39,10 +41,25 @@ logFile 			= createAbsolutePath(logFile)
 logging.basicConfig(filename=logFile,level=logging.ERROR,format='%(asctime)s %(levelname)-8s %(message)s')
 
 #Load config
-config['local']			= json.load(local_path)
-config['server'] 		= json.load(server_path)
-config['supermarkets'] 	= json.load(monitored_path)
+config = dict()
+try:
+	path = local_path
+	with open(path) as json_file:
+		config['local']			= json.load(json_file)
+	path = server_path
+	with open(path) as json_file:
+		config['server'] 		= json.load(json_file)
+	path = monitored_path
+	with open(path) as json_file:
+		config['supermarkets'] 	= json.load(json_file)
+except ValueError:
+	print("Cannot load settings - Invalid json ["+str(path)+"]")
+	exit()
+except FileNotFoundError:
+	print("Cannot load settings - Setting file not found ["+str(path)+"]")
+	exit()
 
+logging.getLogger().setLevel('INFO')
 nfb = NoFilaBot(config, logging)
 
 #On boot action
@@ -50,10 +67,18 @@ if len(sys.argv) > 1 and sys.argv[1]=='systemd':
     logging.info("Started by systemd using argument: "+sys.argv[1])
 
 #Schedule actions
-schedule.every(config['local']['refresh_rate']).minutes.do(nfb.updateStatus())
+schedule.every(config['local']['refresh_rate']).minutes.do(nfb.updateStatus)
+logging.info("Update every "+str(config['local']['refresh_rate'])+" minutes")
+# ~ schedule.every().minutes.do(nfb.updateStatus)
 
 #Start bot
 nfb.start()
+
+#DELETE - FOR Debug only
+# ~ nfb.updateStatus()
+# ~ exit()
+
+
 print('Bot started succesfully')
 logging.info("Bot started succesfully")
 
